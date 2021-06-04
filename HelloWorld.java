996135540ec92d677e89806de26f8e285af84313
deepakreddygit <84923397+deepakreddygit@users.noreply.github.com>
2021-06-04 15:47:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,3 +5,19 @@ public class HelloWorld
 		System.out.println("Welcome to java");
 	}
 }
+class A
+{
+	int x;
+	int y;
+	int z;
+	void add(int a,int b)
+	{
+		x=a;
+		y=b;
+		z=a+b;
+		System.out.println("Addition : "+z);
+	}
+	A obj = new A();
+	obj.add(5,6);
+		
+}	
